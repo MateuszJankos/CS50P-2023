636d781dfca5c0cacf5e06c445a7eee4a72db6aa
MateuszJankos <jankowskim1002@gmail.com>
2023-09-22 16:03:47 +0200

    Updated Hello

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("hello, world!")
+#{name =} pobiera to co jest po prawej od = 
+name = input("Jak masz na imie? ")
+
+# teraz można wykorzystać name poza " " tylko trzeba dodać , lub + 
+# , dodaje spacje
+# + nie dodaje spacji
+print("Witam witam, ", name) 
+print("Witam witam, " + name)
+
+"""
+Nasz komentaż tylko większy 
+"""
+
