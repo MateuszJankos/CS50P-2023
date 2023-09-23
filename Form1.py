6bf99334702914f9caf20a1561e7c4c58564c9a8
MateuszJankos <jankowskim1002@gmail.com>
2023-09-23 13:45:05 +0200

    simple Form

---
+++ Form1.py
@@ -0,0 +1,8 @@
+# zapytaj użytkownika o jego imie, nazwisko, wiek
+name = input("Jak masz na imie? ")
+surname = input("Jak masz na nazwisko? ")
+age = input("Ile masz lat? ")
+
+# teraz użyj tych danych aby stworzyć profil użytkownika
+print("Dziekujemy za udzielone informacje. \t")
+print("Imie: " + name + "   Nazwisko: " + surname + "    Wiek: " + age)
