9f668ca924c8a457fa87de8159fc81ba09b81cf9
MateuszJankos <jankowskim1002@gmail.com>
2023-09-24 21:12:08 +0200

    Update Form2.py

--- Form2.py
+++ Form2.py
@@ -1,2 +1,6 @@
+#Zapytaj użytkownika o jego ulubione rzeczy
 warzywo = input("Jakie jest twoje ulubione warzywo? ")
 Owoc = input("Jakie jest twoje ulubiony owoc? ")
+miesiac = input("Jakie jest twoj ulubiony miesiac? ")
+
+#Zrób profil ulubionych rzeczy użytkownika
