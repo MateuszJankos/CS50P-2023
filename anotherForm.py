c728c45ff27a0ecbe4b6268a4a60e180e6bf162c
MateuszJankos <jankowskim1002@gmail.com>
2023-10-04 19:37:04 +0200

    Wszystko jest już dobrze

---
+++ anotherForm.py
@@ -0,0 +1,4 @@
+print("Jak dobrze znów mieć dostęp do wszystkiego")
+print("Już myślałem że zwariuje tam")
+samopoczucie = input("Powiedz jak się czujesz teraz:   ")
+print("Samopoczucie:  " + samopoczucie)
